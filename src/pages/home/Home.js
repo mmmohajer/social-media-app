ce316137ee94bd9ffcbc240a502a7e07d8f49171
mmmohajer <mmmohajer70@gmail.com>
2021-09-06 14:43:47 -0400

    Main container is ready

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+import Feed from "../../components/feed/Feed";
+import Leftside from "../../components/leftside/Leftside";
+import Rightside from "../../components/rightside/Rightside";
 import Topbar from "../../components/topbar/Topbar";
 import "./home.css";
 
 const Home = () => {
-  return <Topbar />;
+  return (
+    <React.Fragment>
+      <Topbar />
+      <div className="homeContainer">
+        <Leftside />
+        <Feed />
+        <Rightside />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Home;
