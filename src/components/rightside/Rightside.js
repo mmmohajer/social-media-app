3fa83509f2fd333ce278ad5433018985645ccd49
mmmohajer <mmmohajer70@gmail.com>
2021-09-09 11:12:34 -0400

    Rightbar hardcoded completed

--- src/components/rightside/Rightside.js
+++ src/components/rightside/Rightside.js
@@ -5,7 +5,57 @@ import "./rightside.css";
 const Rightside = () => {
   return (
     <div className="rightside">
-      <p>Right Side</p>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img
+            src="/assets/gift.png"
+            alt="Birthday Image"
+            className="birthdayImg"
+          />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="Advertisement" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg"
+                alt="Profile Image"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg"
+                alt="Profile Image"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg"
+                alt="Profile Image"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
